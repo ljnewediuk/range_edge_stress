cc9c3c7bab2ac45045e1b788609b740de516077b
ljnewediuk <j.newedi@gmail.com>
2025-09-30 09:36:22 -0600

    Create outline.R

---
+++ R/outline.R
@@ -0,0 +1,13 @@
+
+# To do:
+#   Identify all species with hormone data
+#   Decide how to deal with stressor-exposed/not
+#   Decide whether to use max cort or late stress cort
+#   Measure distance from observation to range edge by species
+
+# Hypotheses:
+#   Stress increases near range edge (b/c of novel conditions)
+#   Stress decreases near range edge (b/c lower density populations)
+
+# Model:
+#   Stress ~ log(distance range edge) + (log(distance range edge) | spp)
